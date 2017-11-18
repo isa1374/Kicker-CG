46ca706f6ffc76f184b4b6d039eec514d5ebbf0a
isa1374 <isha13gp@hotmail.com>
2017-11-18 16:28:50 -0600

    Ball into object as established in requirements

---
+++ CG_LIGHTING_7/header/Ball.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifdef __APPLE__
+	#include <OpenGL/gl.h>
+	#include <OpenGL/glu.h>
+	#include <GLUT/glut.h>
+#else
+	#include "freeglut.h"
+#endif
+
+class Ball {
+	public:
+		Ball(); 
+		~Ball(); 
+
+		void draw(); 
+};
